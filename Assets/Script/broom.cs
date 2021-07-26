e42b9cea0d1d471d9b25f9ff7ca7501840ac05d4
GaWei <x981235xyvw@gmail.com>
2021-07-26 18:42:52 +0800

    login page

--- Assets/Script/broom.cs
+++ Assets/Script/broom.cs
@@ -15,20 +15,20 @@ public class broom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ( Input.GetKey("up")) {  transform.Translate(0 , 0.1f, 0);  }
+        if ( Input.GetKey("up")) {  transform.Translate(5f, 0, 0);  }
         // 按住 上鍵 時，物件每個 frame 朝自身 X 軸方向移動 0.1 公尺
 
-        if ( Input.GetKey("down")) {  transform.Translate(0 , -0.1f, 0);  }
+        if ( Input.GetKey("down")) {  transform.Translate(-5f, 0, 0);  }
         // 按住 下鍵 時，物件每個 frame 朝自身 X 軸方向移動 -0.1 公尺
 
-        if ( Input.GetKey("left")) {  transform.Rotate( 0.3f, 0, 0);  }
+        if ( Input.GetKey("left")) {  transform.Rotate(0, -0.5f, 0);  }
         // 按住 左鍵 時，物件每個 frame 以自身 y 軸為軸心旋轉 -3 度
 
-        if ( Input.GetKey("right")) {  transform.Rotate( -0.3f, 0, 0);  }        
+        if ( Input.GetKey("right")) {  transform.Rotate(0, 0.5f, 0);  }        
 
-        if ( Input.GetKey("z")) {  transform.Translate(-0.1f , 0, 0);  }
+        if ( Input.GetKey("z")) {  transform.Translate(0, 1f, 0);  }
 
-        if ( Input.GetKey("x")) {  transform.Translate(0.1f , 0, 0);  }
+        if ( Input.GetKey("x")) {  transform.Translate(0 , -1f, 0);  }
         
     }
 }
